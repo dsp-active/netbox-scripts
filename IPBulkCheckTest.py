b7dc68cf923318eac4351e84d1e9a0670bf5232b
dsp-active <danny.speicher@active-logistics.com>
2024-08-01 15:55:00 +0200

    added IPBulkCheck Script

---
+++ IPBulkCheckTest.py
@@ -0,0 +1,18 @@
+from extras.scripts import *
+
+from ipam.models import IPAddress
+from ipam.choices import IPAddressStatusChoices
+
+class IPBulkCheckTest(Script):
+    class Meta:
+        name = "IP Bulk Check Tester"
+        description = "Test every IP address and its corresponding dns-name against each other"
+        commit_default = False
+
+    def run(self, data, commit):
+
+        for addresses in IPAddress.objects.filter(status=IPAddressStatusChoices.STATUS_ACTIVE):
+            if address.dns_name is not None:
+                self.log_success(f"{address.dns_name} belongs to {address}")
+            else:
+                self.log_failure(f"{address} does not have a set DNS name.")
