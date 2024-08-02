255b1b89910341c1a2d5bf859323664dff14d211
dsp-active <danny.speicher@active-logistics.com>
2024-08-02 15:05:48 +0200

    timeout!

--- IPBulkCheckWithPing.py
+++ IPBulkCheckWithPing.py
@@ -20,7 +20,7 @@ class IPBulkCheckWithPing(Script):
                 self.log_info(f"Testing {address.dns_name} against {address} ...")
                 
                 # test ping
-                check = ping(str(address.dns_name), count=3, interval=0.2, privileged=False)
+                check = ping(str(address.dns_name), count=3, interval=0.2, timeout=2, privileged=False)
                 
                 # compare dns-ip to registered ip
                 if str(address.address) in str(check):
