59cbe538be669e4851ec1f970f76d7ef845986af
dsp-active <danny.speicher@active-logistics.com>
2024-08-02 12:35:23 +0200

    Script Update

--- IPChoiceCheckTest.py
+++ IPChoiceCheckTest.py
@@ -8,22 +8,26 @@ from ipam.choices import IPAddressStatusChoices
 class IPChoiceCheckTest(Script):
     class Meta:
         name = "IP Check Tester"
-        description = "Test a given IP address and its corresponding dns-name against each other"
+        description = "Test given IP addresses and its corresponding dns-name against each other"
         commit_default = False
 
-    ip_choice = ObjectVar(
-        description = "Choose an IP Address",
+    ip_choice = MultiObjectVar(
+        description = "Choose one or more IP Addresses",
         model = IPAddress,
-        required = True
+        required = True,
+        query_params={
+            "status": IPAddressStatusChoices.STATUS_ACTIVE
+        }
     )
 
     def run(self, data, commit):
 
         self.log_info(f"{data['ip_choice']} ausgewählt.")
 
-        ip = IPAddress.objects.get(address=data['ip_choice'])
+        #ip = IPAddress.objects.get(address=data['ip_choice'])
 
-        if ip.dns_name is not None and ip.dns_name != "":
-            self.log_success(f"{ip.dns_name} belongs to {ip}")
-        else:
-            self.log_warning(f"{ip} does not have a set DNS name.")
+        for ip in data['ip_choice']:
+            if ip.dns_name is not None and ip.dns_name != "":
+                self.log_success(f"{ip.dns_name} belongs to {ip}")
+            else:
+                self.log_warning(f"{ip} does not have a set DNS name.")
