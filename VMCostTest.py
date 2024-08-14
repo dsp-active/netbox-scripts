a6ca1f13052ad308d1650bc5054727af01cbac19
dsp-active <danny.speicher@active-logistics.com>
2024-08-14 15:27:51 +0200

    added storage calc

--- VMCostTest.py
+++ VMCostTest.py
@@ -8,7 +8,7 @@ from virtualization.choices import VirtualMachineStatusChoices
 class VMCostTest(Script):
     class Meta:
         name = "Test Calculation of VM ressource costs"
-        description = "Calculate a given VMs cost based on its used ressources like vCores and RAM."
+        description = "Calculate a given VMs monthly cost based on its assigned hardware ressources."
         commit_default = False
         
     vm_choice = MultiObjectVar(
@@ -23,13 +23,19 @@ class VMCostTest(Script):
     # there is no Float or DecimalVar for whatever reason~
     vcore_price = StringVar(
         description = "Cost for a single assigned vCore in the following format: X.XX",
-        default = "1.00",
+        default = "1.50",
         min_length = 4,
         regex = "^[0-9]+[.]{1}[0-9]{2}" # starts with 1 or more numbers followed by 1 dot and 2 more numbers
     )
     ram_price = StringVar(
         description = "Cost for a single assigned GB of RAM in the following format: X.XX",
         default = "1.00",
+        min_length = 4,
+        regex = "^[0-9]+[.]{1}[0-9]{2}"
+    )
+    storage_price = StringVar(
+        description = "Cost for a single assigned GB of storage space in the following format: X.XX",
+        default = "0.50",
         min_length = 4,
         regex = "^[0-9]+[.]{1}[0-9]{2}"
     )
@@ -42,11 +48,14 @@ class VMCostTest(Script):
         
         # cost calc
         for vm in data['vm_choice']:
+            # calc vCores
             if vm.vcpus is not None and vm.vcpus != "":
                 self.log_info(f"{vm} has {vm.vcpus} vCores assigned to it.")
-                calcCores = "{:.2f}".format(float(vcore_price * float(vm.vcpus))) # 2 digit float is kind of unnecessary here ^^
+                calcCores = "{:.2f}".format(float(vcore_price * float(vm.vcpus))) # 2 digit float is kind of unnecessary but jic
                 self.log_success(f"{vm.vcpus} vCore/s cost/s {calcCores}€ per month.")
-                total = float(calcCores)
+                total = float(calcCores) # .format -> String var
+                
+                # calc RAM
                 if vm.memory is not None and vm.memory != "":
                     memX = str(vm.memory)[:-3] # zB 4096 -> 4, 16.384 -> 16, 512 -> "" ^^
                     if memX == "":
@@ -57,6 +66,17 @@ class VMCostTest(Script):
                     total += float(calcRam)
                 else:
                     self.log_warning(f"{vm} does not have a set amount of RAM!")
+                
+                # calc storage
+                if vm.disk is not None and vm.disk != "":
+                    self.log_info(f"{vm} has {vm.disk}GBs of storage space assigned to it.")
+                    calcStorage = "{:.2f}".format(float(storage_price * float(vm.disk))
+                    self.log_success(f"{vm.disk}GBs of storage space cost {calcStorage}€ per month.")
+                    total += float(calcStorage)
+                else:
+                    self.log_warning(f"{vm} does not have a set amount of storage!")
+                
+                # output
                 self.log_success(f"#{vm} therefore costs {total}€ in total per month.")
             else:
                 self.log_warning(f"{vm} does not have a set amount of vCores!")
