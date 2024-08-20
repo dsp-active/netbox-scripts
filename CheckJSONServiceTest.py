998c1be41acd07a97dcc7ab2bcc355b7208e2ded
dsp-active <danny.speicher@active-logistics.com>
2024-08-20 11:50:21 +0200

    Added JSON Schema Test

---
+++ CheckJSONServiceTest.py
@@ -0,0 +1,36 @@
+from extras.scripts import *
+
+from virtualization.models import VirtualMachine
+from virtualization.choices import VirtualMachineStatusChoices
+from ipam.models import Service
+
+#from jsonschema import validate
+
+#--------------------
+
+class CheckJSONServiceTest(Script):
+    class Meta:
+        name = "Test the JSON Config of chosen services"
+        description = "Use the JSON Schema library to check the JSON content of the config custom field."
+        commit_default = False
+    
+    vm_choice = MultiObjectVar(
+        description = "Choose a VM",
+        model = VirtualMachine,
+        required = True,
+        query_params={
+            "status": VirtualMachineStatusChoices.STATUS_ACTIVE
+        }
+    )
+
+    #--------------------
+
+    def run(self, data, commit):
+
+        # get & check services
+        for vm in data['vm_choice']:
+            test = vm ( custom_field_data )
+            print(test)
+        
+        # formatting
+        self.log_info(f"--------------------------")
