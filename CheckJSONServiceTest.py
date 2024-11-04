ac21f88f681fc2dbebf317b4e19c0f7fa8204fb4
dsp-active <danny.speicher@active-logistics.com>
2024-11-04 14:56:29 +0100

    Update Validation

--- CheckJSONServiceTest.py
+++ CheckJSONServiceTest.py
@@ -36,29 +36,36 @@ class CheckJSONServiceTest(Script):
         }
     )
 
-    #--------------------
+#--------------------
 
-    def run(self, data, commit):
-        
-        for vm in data['vm_choice']:
-            # get all services that match the chosen vm
-            services = Service.objects.filter(virtual_machine=vm.id)
-            
-            for s in services:
-                # list services
-                self.log_info(f"{vm} has the following service: {s}")
-                
-                # Check custom data, get config & validate against scheme
-                # .get_custom_fields() -> dict mit nicht ansprechbaren keys (<CustomField: JSON Config>)
-                # .custom_fields -> QuerySet - zieht die Felder, aber hab keine Werte >_>
-                customData = s.get_custom_fields()
-                self.log_info(f"custom data: {customData}")
-                customDataX = ", ".join("=".join((str(k), str(v))) for k, v in customData.items())
-                # self.log_info(f"cd as String: {customDataX}")
-                customDataX = ("{" + (customDataX.split('{')[1]).split('}')[0] + "}").replace("'", '"')
-                # self.log_info(f"json conf: {customDataX}")
-                customData = json.loads(customDataX)
-                self.log_info(f"conf: {customData}")
+def run(self, data, commit):
 
-                # formatting
-                self.log_info(f"--------------------------")
+    for vm in data['vm_choice']:
+        # get all services that match the chosen vm
+        services = Service.objects.filter(virtual_machine=vm.id)
+
+        for s in services:
+            # list services
+            self.log_info(f"{vm} has the following service: {s}")
+
+            # Check custom data, get config & validate against scheme
+            # .get_custom_fields() -> dict mit nicht ansprechbaren keys (<CustomField: JSON Config>)
+            # .custom_fields -> QuerySet - zieht die Felder, aber hab keine Werte >_>
+            customData = s.get_custom_fields()
+            # self.log_info(f"custom data: {customData}")
+            customDataX = ", ".join("=".join((str(k), str(v))) for k, v in customData.items())
+            # self.log_info(f"cd as String: {customDataX}")
+            customDataX = ("{" + (customDataX.split('{')[1]).split('}')[0] + "}").replace("'", '"')
+            # self.log_info(f"json conf: {customDataX}")
+            cfg = json.loads(customDataX)
+            self.log_info(f"conf: {cfg}")
+
+            # validate against scheme
+            jsonval = validate(instance=cfg, schema=schema)
+            if jsonval != "":
+                self.log_success(f"Config stimmt mit den Vorgaben überein.")
+            else:
+                self.log_warning(f"Config ist fehlerhaft! Bitte prüfen!")
+
+            # formatting
+            self.log_info(f"--------------------------")
