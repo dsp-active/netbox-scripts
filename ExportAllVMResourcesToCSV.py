e463340659f44407a46a6682a32ca0751536f658
dsp-active <danny.speicher@active-logistics.com>
2024-12-19 11:58:15 +0100

    reverse tenants list fix

--- ExportAllVMResourcesToCSV.py
+++ ExportAllVMResourcesToCSV.py
@@ -81,7 +81,7 @@ class ExportAllVMResourcesToCSV(Script):
         tenants = []
         for tenant in Tenant.objects.all():
             tenants.append(TenantCalc(tenant.id, tenant.name, 0, 0, 0))
-        #tenants = tenants.reverse() # reverse order
+        #tenants = tenants.reverse() # reverse order -> NOPE, breaks typing ^^
         tenants = list(reversed(tenants))
         self.log_info(f"Tenants collected.")
 
