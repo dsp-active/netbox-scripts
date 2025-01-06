6ccc764771d27a9f74c6e1df356ad031a7c9a139
dsp-active <danny.speicher@active-logistics.com>
2025-01-06 11:18:53 +0100

    logging, bug hunting

--- ExportAllVMResourcesToXLSX.py
+++ ExportAllVMResourcesToXLSX.py
@@ -83,7 +83,10 @@ class ExportAllVMResourcesToXLSX(Script):
             tenants.append(TenantCalc(tenant.id, tenant.name, 0, 0, 0))
         #tenants = tenants.reverse() # reverse order -> NOPE, breaks typing ^^
         tenants = list(reversed(tenants))
-        self.log_info(f"Tenants collected.\n{tenants}")
+        self.log_info(f"Tenants collected.")
+        for t in tenants:
+            test = "Name: ", t.get_name(), " - ID: ", t.get_id()
+            self.log_info(test)
 
         # iterate through active VMs and add resources to tenants
         for vm in VirtualMachine.objects.filter(status=VirtualMachineStatusChoices.STATUS_ACTIVE):
